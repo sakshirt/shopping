d7797e9a091e574c2ee2c4f35bc4c071fede225b
Sakshi Rawat <sakshirt10@gmail.com>
2019-10-07 22:59:28 +0530

    Fix

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 
     public function __construct()
     {
-        $this->data = new stdClass();
+        $this->data = new \stdClass();
     }
     /**
      * Register the user
