1d1c76618b08e9c490a4c853119ae7336c7ebe04
Sakshi <sakshirt10@gmail.com>
2020-04-08 18:12:59 +0530

    Home page update
    updated details

--- Modules/Admin/Resources/views/frontend/common/hometopbar.blade.php
+++ Modules/Admin/Resources/views/frontend/common/hometopbar.blade.php
@@ -3,7 +3,7 @@
 
 			<div class="header_agileits">
 				<div class="logo">
-					<h1><a class="navbar-brand" href="index.html"><span>Downy</span> <i>Shoes</i></a></h1>
+					<h1><a class="navbar-brand" href="{{ url('/shop') }}"><span>Divine</span> <i>Impex</i></a></h1>
 				</div>
 				<div class="overlay overlay-contentpush">
 					<button type="button" class="overlay-close"><i class="fa fa-times" aria-hidden="true"></i></button>
@@ -18,11 +18,11 @@
 						</ul>
 					</nav>
 				</div>
-				<div class="mobile-nav-button">
+				<!-- <div class="mobile-nav-button">
 					<button id="trigger-overlay" type="button"><i class="fa fa-bars" aria-hidden="true"></i></button>
-				</div>
+				</div> -->
 				<!-- cart details -->
-				<div class="top_nav_right">
+				<!-- <div class="top_nav_right">
 					<div class="shoecart shoecart2 cart cart box_1">
 						<form action="#" method="post" class="last">
 							<input type="hidden" name="cmd" value="_cart">
@@ -31,10 +31,10 @@
 						</form>
 					</div>
 
-				</div>
+				</div> -->
 				<!-- //cart details -->
 				<!-- search -->
-				<div class="search_w3ls_agileinfo">
+				<!-- <div class="search_w3ls_agileinfo">
 					<div class="cd-main-header">
 						<ul class="cd-header-buttons">
 							<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
@@ -45,7 +45,7 @@
 							<input name="Search" type="search" placeholder="Click enter after typing...">
 						</form>
 					</div>
-				</div>
+				</div> -->
 				<!-- //search -->
 
 				<div class="clearfix"></div>
--- Modules/Admin/Resources/views/frontend/layouts/footer.blade.php
+++ Modules/Admin/Resources/views/frontend/layouts/footer.blade.php
@@ -1,50 +1,56 @@
 <!-- footer -->
 <div class="footer_agileinfo_w3">
 		<div class="footer_inner_info_w3ls_agileits">
-			<div class="col-md-3 footer-left">
-				<h2><a href="index.html"><span>D</span>owny Shoes </a></h2>
-				<p>Lorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>
-				<ul class="social-nav model-3d-0 footer-social social two">
-					<li>
-						<a href="#" class="facebook">
-							<div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-							<div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="twitter">
-							<div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-							<div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="instagram">
-							<div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-							<div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div>
-						</a>
-					</li>
-					<li>
-						<a href="#" class="pinterest">
-							<div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-							<div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div class="col-md-9 footer-right">
+			<div class="col-md-6 footer-left">
 				<div class="sign-grds">
-					<div class="col-md-4 sign-gd">
+					<div class="col-md-12 sign-gd">
+						<h2><a href="index.html"><span>D</span>ivine Impex </a></h2>
+						<p>As a fashion jewelry manufacturer and exporters, DIVINE IMPEX is your reliable expert when it comes to multi faceted jewelry for every season and need. </p>
+						<p>Be inspired by the extensive range in our collection..!</p>
+						<ul class="social-nav model-3d-0 footer-social social two">
+							<li>
+								<a href="#" class="facebook">
+									<div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+									<div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="twitter">
+									<div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+									<div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="instagram">
+									<div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+									<div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div>
+								</a>
+							</li>
+							<li>
+								<a href="#" class="pinterest">
+									<div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+									<div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 footer-right">
+				<div class="sign-grds">
+					<div class="col-md-6 sign-gd">
 						<h4>Our <span>Information</span> </h4>
 						<ul>
 							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
+							<li><a href="shop.html">Shop</a></li>
+							<!-- <li><a href="about.html">About</a></li>
 							<li><a href="404.html">Services</a></li>
 							<li><a href="404.html">Short Codes</a></li>
-							<li><a href="contact.html">Contact</a></li>
+							<li><a href="contact.html">Contact</a></li> -->
 						</ul>
 					</div>
 
-					<div class="col-md-5 sign-gd-two">
+					<div class="col-md-6 sign-gd-two">
 						<h4>Store <span>Information</span></h4>
 						<div class="address">
 							<div class="address-grid">
@@ -53,7 +59,7 @@
 								</div>
 								<div class="address-right">
 									<h6>Phone Number</h6>
-									<p>+1 234 567 8901</p>
+									<p>+91 8130533182</p>
 								</div>
 								<div class="clearfix"> </div>
 							</div>
@@ -63,7 +69,8 @@
 								</div>
 								<div class="address-right">
 									<h6>Email Address</h6>
-									<p>Email :<a href="mailto:example@email.com"> mail@example.com</a></p>
+									<p><a href="mailto:divineimpex@bol.net.in"> divineimpex@bol.net.in</a></p>
+									<p><a href="mailto:divineimpexjewelry@gmail.com"> divineimpexjewelry@gmail.com</a></p>
 								</div>
 								<div class="clearfix"> </div>
 							</div>
@@ -73,15 +80,25 @@
 								</div>
 								<div class="address-right">
 									<h6>Location</h6>
-									<p>Broome St, NY 10002,California, USA.
-
+									<p>L-17A, Second Floor, Malviya Nagar,
+                                            New Delhi 110017, 
+                                            India
 									</p>
 								</div>
 								<div class="clearfix"> </div>
 							</div>
+							<div class="address-grid">
+								<div class="address-left">
+									<i class="fa fa-map-marker" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h6>Site Location</h6>
+									<p>E 4, EPIP, Surajpur Industrial area-Site 5, Greater Noida, UP</p>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="col-md-3 sign-gd flickr-post">
+					<!-- <div class="col-md-3 sign-gd flickr-post">
 						<h4>Flickr <span>Posts</span></h4>
 						<ul>
 							<li><a href="single.html"><img src="{{ getPublicFiles('frontend/images/t1.jpg')}}" alt=" " class="img-responsive" /></a></li>
@@ -94,13 +111,13 @@
 							<li><a href="single.html"><img src="{{ getPublicFiles('frontend/images/t2.jpg')}}" alt=" " class="img-responsive" /></a></li>
 							<li><a href="single.html"><img src="{{ getPublicFiles('frontend/images/t4.jpg')}}" alt=" " class="img-responsive" /></a></li>
 						</ul>
-					</div>
+					</div> -->
 					<div class="clearfix"></div>
 				</div>
 			</div>
 			<div class="clearfix"></div>
 
-			<p class="copy-right-w3ls-agileits">&copy 2018 Downy Shoes. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			<!-- <p class="copy-right-w3ls-agileits">&copy 2020 Divine Impex. All rights reserved</p> -->
 		</div>
 	</div>
 	</div>
